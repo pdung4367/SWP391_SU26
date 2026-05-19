eb8c1cd27bf30e1212f179fcf9ca3de04dc9ac49
Hoanglong91 <blong2294@gmail.com>
2026-05-19 16:05:45 +0700

    feat: add ListingTable component and register in admin features index

--- client/src/features/admin/index.js
+++ client/src/features/admin/index.js
@@ -2,3 +2,5 @@ export { default as AnalyticsPage } from './pages/AnalyticsPage';
 export { default as TransactionsPage } from './pages/TransactionsPage';
 export { default as StatCard } from './components/StatCard';
 export { default as TransactionTable } from './components/TransactionTable';
+export { default as ListingsPage } from './pages/ListingsPage';
+export { default as ListingTable } from './components/ListingTable';
